0ce7029ff064cbcf0cbbc31cee650a25a5b598e1
phuopham <108047552+phuopham@users.noreply.github.com>
2024-07-24 18:46:07 +0700

    move state to parent

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,22 +1,5 @@
-import { AgeForm } from '@/components/age_form'
-import { HeightForm } from '@/components/height_form'
-import { IncomeForm } from '@/components/income_form'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/')({
-    component: Index,
+  component: () => <div>Hello /!</div>
 })
-
-function Index() {
-    return (
-        <div className="p-2">
-            <h3>Welcome Home!</h3>
-            <div>
-                {/* main content */}
-                <AgeForm />
-                <HeightForm />
-                <IncomeForm />
-            </div>
-        </div>
-    )
-}
