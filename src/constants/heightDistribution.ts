b93423dc58f878c50f1e8c63bdb5d00ce888c87a
phuopham <108047552+phuopham@users.noreply.github.com>
2024-07-24 10:21:23 +0700

    standardize the data

--- src/constants/heightDistribution.ts
+++ src/constants/heightDistribution.ts
@@ -1,27 +1,43 @@
 //2020, the average Vietnamese man is 168.1cm tall and the average Vietnamese woman is 156.2cm tall
-// JSON was created with normal distribution
+// JSON was created with normal distribution (ChatGPT)
 const HEIGHT_DISTRIBUTION_RAW = {
-    "gender": {
-      "male": {
-        "1.50-1.54m": 1.0,
-        "1.55-1.59m": 5.0,
-        "1.60-1.64m": 20.0,
-        "1.65-1.69m": 34.0,
-        "1.70-1.74m": 30.0,
-        "1.75-1.79m": 8.0,
-        "1.80-1.84m": 2.0,
-        "1.85-1.89m": 0.5
-      },
-      "female": {
-        "1.50-1.54m": 1.0,
-        "1.55-1.59m": 5.0,
-        "1.60-1.64m": 20.0,
-        "1.65-1.69m": 34.0,
-        "1.70-1.74m": 30.0,
-        "1.75-1.79m": 8.0,
-        "1.80-1.84m": 2.0,
-        "1.85-1.89m": 0.5
-      }
+  "gender": {
+    "male": {
+      "1.50-1.54m": 1.0,
+      "1.55-1.59m": 5.0,
+      "1.60-1.64m": 20.0,
+      "1.65-1.69m": 34.0,
+      "1.70-1.74m": 30.0,
+      "1.75-1.79m": 8.0,
+      "1.80-1.84m": 2.0,
+      "1.85-1.89m": 0.5
+    },
+    // ????
+    "female": {
+      "1.50-1.54m": 1.0,
+      "1.55-1.59m": 5.0,
+      "1.60-1.64m": 20.0,
+      "1.65-1.69m": 34.0,
+      "1.70-1.74m": 30.0,
+      "1.75-1.79m": 8.0,
+      "1.80-1.84m": 2.0,
+      "1.85-1.89m": 0.5
     }
   }
-  
+}
+//base on normal distribution and my assumption
+//TODO: find trusted source
+export const HEIGHT_DISTRIBUTION = [
+  { height: 140, male: 0.2, female: 8 },
+  { height: 150, male: 0.3, female: 10 },
+  { height: 155, male: 0.7, female: 30 },
+  { height: 160, male: 5, female: 34 },
+  { height: 165, male: 20, female: 20 },
+  { height: 170, male: 34, female: 5 },
+  { height: 175, male: 30, female: 2 },
+  { height: 180, male: 8, female: 0.5 },
+  { height: 185, male: 2, female: 0.1 },
+  { height: 190, male: 0.3, female: 0.1 },
+  { height: 195, male: 0.1, female: 0 },
+  { height: 200, male: 0.1, female: 0 }
+] as const
--- src/constants/incomeDistribution.ts
+++ src/constants/incomeDistribution.ts
@@ -13,7 +13,7 @@ const INCOME_DISTRIBUTION_RAW = [
 
 // low and high in million    
 
-const INCOME_DISTRIBUTION = [
+export const INCOME_DISTRIBUTION = [
   {range: "40,000,000 VND and over", low:40, percentage: 7},
   {range: "30,000,000 - 39,999,999 VND",low:30, high:39, percentage: 8},
   {range: "25,000,000 - 29,999,999 VND",low:25, high:29, percentage: 13},
@@ -23,5 +23,5 @@ const INCOME_DISTRIBUTION = [
   {range: "5,000,000 - 9,999,999 VND",low:50, high:9, percentage: 14},
   {range: "Under 4,990,000",high:4, percentage: 3}
 
-]
+] as const
   
