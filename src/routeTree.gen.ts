049cbed27da846446c9e65075799a3dc07e83f35
phuopham <108047552+phuopham@users.noreply.github.com>
2024-07-26 18:03:26 +0700

    fix route fails

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -22,12 +22,12 @@ const IndexLazyImport = createFileRoute('/')()
 // Create/Update Routes
 
 const DataLazyRoute = DataLazyImport.update({
-  path: '/vietnam_standard_calculator/data',
+  path: '/data',
   getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/data.lazy').then((d) => d.Route))
 
 const IndexLazyRoute = IndexLazyImport.update({
-  path: '/vietnam_standard_calculator/',
+  path: '/',
   getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/index.lazy').then((d) => d.Route))
 
