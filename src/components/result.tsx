4c183305d3494893ed0a7f78a0779fcf602c14e5
phuopham <108047552+phuopham@users.noreply.github.com>
2024-07-26 18:16:31 +0700

    update typo

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -5,7 +5,7 @@ export const Result = ({ percentage }: { percentage: number }) => {
         <div className='col-span-2 flex flex-col items-center justify-center px-5 '>
             Kết quả
             <div className='border rounded-xl bg-slate-100 p-5'>
-                {percentage}% dân số phù hợp với mong muốn của bạn. Bạn cần làm quen với tối thiểu {Math.round(1 / percentage).toLocaleString('vi')} người để tìm được đối tượng.
+                {percentage}% dân số phù hợp với mong muốn của bạn. Trung bình, Một trong số {Math.round(1 / percentage).toLocaleString('vi')} người xung quanh bạn đạt tiêu chuẩn.
                 Năm 2024, dân số nước ta là {POPULATION.toLocaleString('vi')}. Bạn có thể tìm được {Math.round((POPULATION * percentage)).toLocaleString('vi')} đối tượng
             </div>
         </div>
