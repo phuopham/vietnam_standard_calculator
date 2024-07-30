a976169aa13ea212c02e2071c246d68aa1aef032
phuopham <108047552+phuopham@users.noreply.github.com>
2024-07-30 21:31:46 +0700

    complete feature but need to style it properly

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -4,25 +4,17 @@ import { GenderType } from "./pages/main"
 import Male from '@/assets/male.svg?react'
 import Female from '@/assets/female.svg?react'
 import { cn } from "@/lib/utils"
-import { useEffect, useState } from "react"
 
-export const Model = ({ key, gender, percentage }: { key: number, gender: GenderType, percentage: number }) => {
-    console.log(key)
+export const Model = ({ id, gender, percentage }: { id: number, gender: GenderType, percentage: number }) => {
+    console.log(percentage)
     return gender == 'male' ?
         (
-            <Male className={cn('mx-[20%] h-7 w-3', percentage >= key ? "" : "bg-emerald-500")} key={key} />
+            <Male className={cn('h-6 w-3', (percentage * 1000) < id ? "" : "bg-emerald-500")} key={id} />
         ) : (
-            <Female className={cn('-mx-[20%] h-7 w-6', percentage >= key ? "" : "bg-emerald-500")} key={key} />
+            <Female className={cn('h-6 w-3', (percentage * 1000) < id ? "" : "bg-emerald-500")} key={id} />
         )
 }
 export const Result = ({ gender, percentage }: { gender: GenderType, percentage: number }) => {
-    const [modelArray, setModelArray] = useState<number[]>([0, 1])
-    // useEffect(() => {
-    //     setModelArray(Array.from({ length: 1000 }, (_, i) => {
-    //         return (i)
-    //     }))
-    // }, [])
-    console.log(modelArray)
     return (
         <div className='col-span-2 flex flex-col items-center justify-center px-5 '>
             Kết quả
@@ -31,8 +23,10 @@ export const Result = ({ gender, percentage }: { gender: GenderType, percentage:
                 Năm 2024, dân số nước ta là {POPULATION.toLocaleString('vi')}. Bạn có thể tìm được {Math.round((POPULATION * percentage)).toLocaleString('vi')} đối tượng
             </div>
             <div className="grid grid-cols-50">
-                {modelArray.map((_, i) =>
-                    <Model key={i} gender={gender} percentage={0} />
+                {Array.from({ length: 1000 }, (_, i) => {
+                    return (i)
+                }).map((i) =>
+                    <Model key={i} id={i} gender={gender} percentage={percentage} />
                 )
                 }
             </div>
